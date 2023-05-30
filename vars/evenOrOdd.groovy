f1e4a38e0c53235a936add882c1928c88b4e58b5
fadia <fadia@XLIM05040.sic.univ-poitiers.fr>
2023-05-30 15:35:33 +0200

    Added build-in pipeline

---
+++ vars/evenOrOdd.groovy
@@ -0,0 +1,25 @@
+def call(int buildNumber) {
+    if (buildNumber % 2 == 0) {
+        pipeline {
+            agent any
+                stages {
+                    stage('Even Stage') {
+                        steps {
+                            
+                        }
+                    }
+                }
+            }
+        } else {
+        pipeline {
+            agent any
+                stages {
+                    stage('Odd Stage') {
+                        steps {
+                            echo "The build number is odd"
+                        }
+                    }
+                }
+        }
+    }
+}
